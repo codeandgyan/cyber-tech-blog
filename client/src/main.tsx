eebb36ddb35bdb5779d2939e66baeddd3277e5d9
Ramalingam S <codeandgyan@gmail.com>
2025-08-10 15:50:30 +0530

    reverting base path changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/cyber-tech-blog">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
